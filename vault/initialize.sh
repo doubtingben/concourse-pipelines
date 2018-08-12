eed97d28b2c5cb85b1c255d3bcf840fb26baf00f
Ben Wilson <doubtingben@gmail.com>
2018-08-12 13:10:53 -0400

    initial commit

---
+++ vault/initialize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export VAULT_TOKEN=rootToken-3434342
+export VAULT_ADDR=http://127.0.0.1:8200
+vault policy write concourse policies/concourse.hcl
+vault secrets enable -path=concourse kv
+vault write concourse/main/docker-hub-email value=bwilson@yooser.org
+vault write concourse/main/docker-hub-username value=doubtingben
